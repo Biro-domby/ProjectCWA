1e8ece62fa2930c633067654fad01a96927a9fb7
biro-domby <biro.developper@gmail.com>
2023-01-29 00:32:28 +0100

    projet cwa

--- api-cwa/app/src/main/java/com/ub/apicwa/dao/RepoCategory.java
+++
@@ -1,9 +0,0 @@
-package com.ub.apicwa.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ub.apicwa.models.Categorie;
-
-public interface RepoCategory extends CrudRepository<Categorie, Integer>{
-
-}
